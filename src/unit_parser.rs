ae908364200215e017250213469b928ae1ec2456
Dan Barella <dan.barella@gmail.com>
2018-01-27 13:24:55 -0500

    Implement barebones type structure.
    My initial idea is pretty naive -- I figure I want to convert a token
    stream into a parse tree of some sort, though the parse tree might not
    end up being that necessary. We'll see where things go from here.

--- src/unit_parser.rs
+++ src/unit_parser.rs
@@ -1,13 +1,33 @@
-fn parse() -> i64 {
-    42
+/// A token derived from the user input
+#[derive(Debug, PartialEq)]
+enum Token {
+    ONE
+}
+
+/// A node in the parse tree
+#[derive(Debug, PartialEq)]
+struct TreeNode<T> {
+    data: T,
+}
+
+/// A parse tree, assembled from a stream of Tokens
+#[derive(Debug, PartialEq)]
+enum ParseTree<T> {
+    Empty,
+    Node(TreeNode<T>),
+}
+
+/// Parses a stream of tokens into a parse tree
+fn parse(token_stream: &[Token]) -> ParseTree<i32> {
+    ParseTree::Empty
 }
 
 #[cfg(test)]
 mod tests {
-    use unit_parser;
+    use unit_parser::{parse, ParseTree};
 
     #[test]
-    fn parse_does_nothing() {
-        assert_eq!(unit_parser::parse(), 42);
+    fn parse_empty_stream_does_nothing() {
+        assert_eq!(parse(&[]), ParseTree::Empty);
     }
 }
