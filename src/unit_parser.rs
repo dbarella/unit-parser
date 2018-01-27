7611c8bbcf3f0743da4ce55d1c2673f18d2d27f1
Dan Barella <dan.barella@gmail.com>
2018-01-27 14:07:34 -0500

    Sketch out more types.
    More progress toward parsing stuff. I should probably move some of these
    types out into another module, like `tokenize`, but I wanted to get
    things working together first.
    
    The type system is starting to make sense -- using num::BigInt is kinda
    a drag performance-wise (and also not what I want API-wise), but I want
    to be able to abstract over all the numeric types where possible, so
    that's why I'm doing that for now.
    
    My ideal situation would be to represent everything as a real, so maybe
    I'll switch to doing that.

--- src/unit_parser.rs
+++ src/unit_parser.rs
@@ -1,24 +1,78 @@
-/// A token derived from the user input
+//! This module is designed to convert a token stream into a parse tree.
+//!
+//! Say that we have the following expression:
+//!
+//! 2 kg * 4 m / 8 s^2
+//!
+//! We would like to reduce this expression to:
+//!
+//! 1 kg * m / s^2
+//!
+//! Or, even better:
+//!
+//! 1 N
+
+extern crate num;
+
+/// Delimeter tokens, e.g:
+/// (1 + 2) s
+/// ^     ^-------- RIGHT_PAREN
+/// |
+/// ---------- LEFT_PAREN
+#[derive(Debug, PartialEq)]
+enum Delimeter {
+    LEFT_PAREN,
+    RIGHT_PAREN,
+}
+
+/// Operator tokens, e.g:
+/// 1 s + 2 s
+///     ^----- PLUS token
+#[derive(Debug, PartialEq)]
+enum Operator {
+    PLUS,
+    MINUS,
+    MUL,
+    DIV,
+    POW
+}
+
+/// Unit tokens, e.g:
+/// 1 s
+///   ^----- SECONDS token
+#[derive(Debug, PartialEq)]
+enum Unit {
+    KILOGRAMS,
+    METERS,
+    SECONDS,
+    NEWTONS,
+}
+
+/// Data for the parse tree.
+#[derive(Debug, PartialEq)]
+struct NumericValue {
+    number: num::BigInt,
+    unit: Unit,
+}
+
+/// A token derived from the user input.
 #[derive(Debug, PartialEq)]
 enum Token {
-    ONE
+    Delimeter(Delimeter),
+    Number(num::BigInt),
+    Operator(Operator),
+    Unit(Unit),
 }
 
-/// A node in the parse tree
-#[derive(Debug, PartialEq)]
-struct TreeNode<T> {
-    data: T,
-}
-
-/// A parse tree, assembled from a stream of Tokens
+/// A parse tree, assembled from a stream of Tokens.
 #[derive(Debug, PartialEq)]
 enum ParseTree<T> {
+    Node(T),
     Empty,
-    Node(TreeNode<T>),
 }
 
-/// Parses a stream of tokens into a parse tree
-fn parse(token_stream: &[Token]) -> ParseTree<i32> {
+/// Parses a stream of tokens into a parse tree.
+fn parse(token_stream: &[Token]) -> ParseTree<Token> {
     ParseTree::Empty
 }
 
